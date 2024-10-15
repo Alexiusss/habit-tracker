c035e667a79dce3c6e2a6a3042214853bd531d44
Alexey Boyarinov <boyarinovalexey@gmail.com>
2024-10-15 16:21:43 +0300

    Added JavaDoc for the exceptions

--- src/main/java/com/example/habittracker/exception/DuplicateEmailException.java
+++ src/main/java/com/example/habittracker/exception/DuplicateEmailException.java
@@ -1,6 +1,23 @@
 package com.example.habittracker.exception;
 
-public class DuplicateEmailException extends RuntimeException{
+
+/**
+ * Exception thrown when a duplicate email is encountered during user registration or updating.
+ * This exception extends {@code RuntimeException}, meaning it is an unchecked exception.
+ *
+ * <p>This could occur when attempting to create or update a user with an email
+ * address that already exists in the system.</p>
+ *
+ * @author Alexey Boyarinov
+ * @see RuntimeException
+ */
+public class DuplicateEmailException extends RuntimeException {
+
+    /**
+     * Constructs a new {@code DuplicateEmailException} with the specified detail message.
+     *
+     * @param message the detail message explaining the reason for the exception
+     */
     public DuplicateEmailException(String message) {
         super(message);
     }
